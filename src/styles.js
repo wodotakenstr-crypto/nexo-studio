9fb25b08b390d4723e2ba5cf1fee9c3043341766
kenneth rubino <wodotakenstr@gmail.com>
2026-08-12 14:39:56 -0400

    Add customer journey shortcuts

--- src/styles.js
+++ src/styles.js
@@ -19,5 +19,6 @@ import '../custom-services.css';
 import '../navigation-fix.css';
 import '../trust.css';
 import '../support.css';
+import '../start-here.css';
 import '../demos/demo.css';
 import '../demos/demo-extra.css';
